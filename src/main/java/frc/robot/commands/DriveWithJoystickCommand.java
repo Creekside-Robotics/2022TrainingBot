f5c6df65d69f1929252aac1720a84802faafee01
ethanglocke <105320939+ethanglocke@users.noreply.github.com>
2022-10-31 08:50:48 -0400

    Update DriveWithJoystickCommand.java

--- src/main/java/frc/robot/commands/DriveWithJoystickCommand.java
+++ src/main/java/frc/robot/commands/DriveWithJoystickCommand.java
@@ -35,6 +35,7 @@ public class DriveWithJoystickCommand extends CommandBase {
   public void execute() {
     double forwardSpeed = RobotContainer.joystick.getX();
     double turningSpeed = RobotContainer.joystick.getZ();
+    this.drivetrainSubsystem.setDrive(forwardSpeed, turningSpeed);
   }
 
   // Called once the command ends or is interrupted.
